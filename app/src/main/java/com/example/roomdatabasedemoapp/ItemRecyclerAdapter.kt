14ca428d8a99b5a5af96fae508a52a7af3fe8fa3
Shahzaib Ahmad <shahrazeahmad07@gmail.com>
2023-06-03 05:56:52 +0500

    Creating and reading data completed

---
+++ app/src/main/java/com/example/roomdatabasedemoapp/ItemRecyclerAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.roomdatabasedemoapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemRecyclerAdapter(private val list: ArrayList<EmployeeEntity>):
+    RecyclerView.Adapter<ItemRecyclerAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_employee_entry, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentEmployee = list[position]
+        holder.tvRvName.text = currentEmployee.name
+        holder.tvRvEmail.text = currentEmployee.email
+        if (position % 2 == 0) {
+            holder.parentLayout.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.gray))
+        } else {
+            holder.parentLayout.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.white))
+        }
+    }
+
+    inner class ViewHolder(itemView: View) :  RecyclerView.ViewHolder(itemView) {
+        val parentLayout: ConstraintLayout
+        val tvRvName: TextView
+        val tvRvEmail: TextView
+        init {
+            parentLayout = itemView.findViewById(R.id.parentLayout)
+            tvRvName = itemView.findViewById(R.id.tvRvName)
+            tvRvEmail = itemView.findViewById(R.id.tvRvEmail)
+        }
+    }
+}
--- app/src/main/java/com/example/roomdatabasedemoapp/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabasedemoapp/MainActivity.kt
@@ -2,24 +2,57 @@ package com.example.roomdatabasedemoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.roomdatabasedemoapp.databinding.ActivityMainBinding
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    var binding: ActivityMainBinding? = null
+    private var binding: ActivityMainBinding? = null
+    private lateinit var itemRecyclerAdapter: ItemRecyclerAdapter
+    private lateinit var listOfEmployees: ArrayList<EmployeeEntity>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
+        //! creating employeeDao object
         val employeeDao = (application as EmployeeApp).db.employeeDao()
 
+
+        //! getting all employees:
+        lifecycleScope.launch {
+            employeeDao.fetchAllEmployee().collect {
+                listOfEmployees = ArrayList(it)
+                itemRecyclerAdapter = ItemRecyclerAdapter(listOfEmployees)
+                showAllEmployees(listOfEmployees)
+            }
+        }
+
+
+
+        //! add record button
         binding?.btnAddRecord?.setOnClickListener {
             addRecord(employeeDao)
         }
 
+    }
+
+    fun showAllEmployees(listOfAllEmployees: ArrayList<EmployeeEntity>) {
+        if (listOfAllEmployees.isNotEmpty()) {
+            //! setting recyclerView:
+            binding?.recyclerView?.layoutManager = LinearLayoutManager(this)
+            binding?.recyclerView?.adapter = itemRecyclerAdapter
+            binding?.tvNoData?.visibility = View.GONE
+            binding?.recyclerView?.visibility = View.VISIBLE
+        } else {
+            binding?.tvNoData?.visibility = View.VISIBLE
+            binding?.recyclerView?.visibility = View.GONE
+        }
     }
 
     private fun addRecord(employeeDao: EmployeeDao) {
