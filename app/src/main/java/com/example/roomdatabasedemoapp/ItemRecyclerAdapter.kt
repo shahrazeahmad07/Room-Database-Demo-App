f0918c1cbe29e0a7d52fb6affcdd5b2afe035c45
Shahzaib Ahmad <shahrazeahmad07@gmail.com>
2023-06-06 10:13:58 +0500

    Update and Delete feature added, app completed

--- app/src/main/java/com/example/roomdatabasedemoapp/ItemRecyclerAdapter.kt
+++ app/src/main/java/com/example/roomdatabasedemoapp/ItemRecyclerAdapter.kt
@@ -3,16 +3,23 @@ package com.example.roomdatabasedemoapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
-class ItemRecyclerAdapter(private val list: ArrayList<EmployeeEntity>):
+class ItemRecyclerAdapter(
+    private val list: ArrayList<EmployeeEntity>,
+    private val updateMethod: (position: Int) -> Unit,
+    private val deleteMethod: (position: Int) -> Unit
+) :
     RecyclerView.Adapter<ItemRecyclerAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_employee_entry, parent, false)
+        val v =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_employee_entry, parent, false)
         return ViewHolder(v)
     }
 
@@ -25,20 +32,41 @@ class ItemRecyclerAdapter(private val list: ArrayList<EmployeeEntity>):
         holder.tvRvName.text = currentEmployee.name
         holder.tvRvEmail.text = currentEmployee.email
         if (position % 2 == 0) {
-            holder.parentLayout.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.gray))
+            holder.parentLayout.setBackgroundColor(
+                ContextCompat.getColor(
+                    holder.itemView.context,
+                    R.color.gray
+                )
+            )
         } else {
-            holder.parentLayout.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.white))
+            holder.parentLayout.setBackgroundColor(
+                ContextCompat.getColor(
+                    holder.itemView.context,
+                    R.color.white
+                )
+            )
+        }
+        holder.ibRvEdit.setOnClickListener {
+            updateMethod.invoke(position)
+        }
+        holder.ibRvDelete.setOnClickListener {
+            deleteMethod.invoke(position)
         }
     }
 
-    inner class ViewHolder(itemView: View) :  RecyclerView.ViewHolder(itemView) {
-        val parentLayout: ConstraintLayout
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val parentLayout: LinearLayout
         val tvRvName: TextView
         val tvRvEmail: TextView
+        val ibRvEdit : ImageButton
+        val ibRvDelete: ImageButton
+
         init {
             parentLayout = itemView.findViewById(R.id.parentLayout)
             tvRvName = itemView.findViewById(R.id.tvRvName)
             tvRvEmail = itemView.findViewById(R.id.tvRvEmail)
+            ibRvEdit = itemView.findViewById(R.id.ibRvEdit)
+            ibRvDelete = itemView.findViewById(R.id.ibRvDelete)
         }
     }
 }
--- app/src/main/java/com/example/roomdatabasedemoapp/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabasedemoapp/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.example.roomdatabasedemoapp
 
+import android.app.AlertDialog
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.roomdatabasedemoapp.databinding.ActivityMainBinding
+import com.example.roomdatabasedemoapp.databinding.DialogBinding
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private var binding: ActivityMainBinding? = null
     private lateinit var itemRecyclerAdapter: ItemRecyclerAdapter
     private lateinit var listOfEmployees: ArrayList<EmployeeEntity>
+
+    // Dialog ki layout set or wese recycler view k item view ki layout set kerni hai dono ki kerni hai
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,11 +35,15 @@ class MainActivity : AppCompatActivity() {
         lifecycleScope.launch {
             employeeDao.fetchAllEmployee().collect {
                 listOfEmployees = ArrayList(it)
-                itemRecyclerAdapter = ItemRecyclerAdapter(listOfEmployees)
+                itemRecyclerAdapter = ItemRecyclerAdapter(listOfEmployees,
+                    { position ->
+                        showEditUpdateDialog(position, employeeDao)
+                    }, { position ->
+                        showDeleteDialog(position, employeeDao)
+                    })
                 showAllEmployees(listOfEmployees)
             }
         }
-
 
 
         //! add record button
@@ -42,7 +53,53 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun showAllEmployees(listOfAllEmployees: ArrayList<EmployeeEntity>) {
+    private fun showEditUpdateDialog(position: Int, employeeDao: EmployeeDao) {
+        val dialog = Dialog(this, R.style.ThemeDialog)
+        val dialogBinding = DialogBinding.inflate(layoutInflater)
+        dialog.setContentView(dialogBinding.root)
+        dialog.setTitle("Update Entry")
+        dialogBinding.etDialogName.setText(listOfEmployees[position].name)
+        dialogBinding.etDialogEmail.setText(listOfEmployees[position].email)
+        dialogBinding.btnDialogUpdate.setOnClickListener {
+            lifecycleScope.launch {
+                val name = dialogBinding.etDialogName.text.toString()
+                val email = dialogBinding.etDialogEmail.text.toString()
+                if (name.isNotEmpty() && email.isNotEmpty()) {
+                    employeeDao.update(EmployeeEntity(listOfEmployees[position].id, name, email))
+                    Toast.makeText(this@MainActivity, "Record Updated!!", Toast.LENGTH_SHORT).show()
+                    dialog.dismiss()
+                } else {
+                    Toast.makeText(this@MainActivity, "Enter all fields!!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        }
+        dialogBinding.btnDialogCancel.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.setCancelable(false)
+        dialog.show()
+    }
+
+    private fun showDeleteDialog(position: Int, employeeDao: EmployeeDao) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        val name = listOfEmployees[position].name
+        builder.setTitle("Delete")
+        builder.setMessage("Are you sure you want to delete entry: $name")
+        builder.setPositiveButton("Yes") { dialog, _ ->
+            lifecycleScope.launch {
+                employeeDao.delete(listOfEmployees[position])
+                dialog.dismiss()
+                Toast.makeText(this@MainActivity, "Entry Deleted!!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        builder.setNegativeButton("Cancel") { dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.create().show()
+    }
+
+    private fun showAllEmployees(listOfAllEmployees: ArrayList<EmployeeEntity>) {
         if (listOfAllEmployees.isNotEmpty()) {
             //! setting recyclerView:
             binding?.recyclerView?.layoutManager = LinearLayoutManager(this)
@@ -65,7 +122,7 @@ class MainActivity : AppCompatActivity() {
                 binding?.etEmail?.text?.clear()
                 Toast.makeText(applicationContext, "Record Added!!", Toast.LENGTH_SHORT).show()
             }
-        }else {
+        } else {
             Toast.makeText(this, "Enter all fields!!", Toast.LENGTH_SHORT).show()
         }
     }
